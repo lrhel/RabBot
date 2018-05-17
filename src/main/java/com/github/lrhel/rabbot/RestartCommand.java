fd47b206630b2c8a5b10468b9aa8202f53917a99
lrhel <lrhel@users.noreply.github.com>
2018-05-17 19:22:11 +0200

    ...

--- src/main/java/com/github/lrhel/rabbot/RestartCommand.java
+++ src/main/java/com/github/lrhel/rabbot/RestartCommand.java
@@ -4,15 +4,13 @@ import java.io.IOException;
 
 import org.javacord.api.entity.user.User;
 
-import com.github.lrhel.rabbot.config.Config;
-
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 
 public class RestartCommand implements CommandExecutor {
 	@Command(aliases = {"restart"}, privateMessages = false, description = "Restart RabBot!", showInHelpPage = false)
 	public String onRestartCommand(User usr) {
-		if(usr.isBotOwner() || usr.getIdAsString().contentEquals(Config.SMATHID.toString())) {
+		if(usr.isBotOwner()) {
 			new Thread(new Runnable() {
 				public void run(){
 					try {
