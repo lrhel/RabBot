3a3944a2276e8b5cf4fa2b672297378e75147f69
lrhel <lrhel@users.noreply.github.com>
2018-07-22 16:03:48 +0200

    Inventory typo

--- src/main/java/com/github/lrhel/rabbot/command/pokemon/InventoryCommand.java
+++ src/main/java/com/github/lrhel/rabbot/command/pokemon/InventoryCommand.java
@@ -17,9 +17,9 @@ public class InventoryCommand implements CommandExecutor {
     @Command(aliases = {"inventory", "inv"}, description = "Pokemon inventory")
     public String onCommand(User user, TextChannel textChannel){
         try {
-            final String sql = "SELECT PKMN_NAME, COUNT(*) as NB FROM CATCH WHERE DISCORD_ID = ? GROUP BY PKMN_NAME LIMIT ? OFFSET ?";
+            final String sql = "SELECT PKMN, PKMN_NAME, COUNT(*) as NB FROM CATCH WHERE DISCORD_ID = ? GROUP BY PKMN_NAME ORDER BY PKMN LIMIT ? OFFSET ?";
             final int limit = 20;
-            String result = "";
+            String result = "```css\n";
             Offset offset = new Offset();
 
             Connection c = Sqlite.getInstance().getConnection();
@@ -29,15 +29,19 @@ public class InventoryCommand implements CommandExecutor {
             pstmt.setInt(3, offset.getOffset());
             ResultSet rs = pstmt.executeQuery();
             while(rs.next()){
-                result += rs.getString("PKMN_NAME") + " x" + rs.getInt("NB") + "\n";
+                result += "#" + rs.getString("PKMN");
+                result += Integer.parseInt(rs.getString("PKMN")) < 100 ? " " : "";
+                result += Integer.parseInt(rs.getString("PKMN")) < 10 ? " " : "";
+                result += " " + rs.getString("PKMN_NAME") + " x" + rs.getInt("NB") + "\n";
             }
+            result += "```";
             Message msg = textChannel.sendMessage(result).get();
             msg.addReaction(EmojiParser.parseToUnicode(":arrow_left:")); //left arrow
             msg.addReaction(EmojiParser.parseToUnicode(":arrow_right:")); //right arrow
             msg.addReaction(EmojiParser.parseToUnicode(":x:"));//X cross❌❌ ❌
             msg.addReactionAddListener(event -> {
                 try {
-                    String result1 = "";
+                    String result1;
                     PreparedStatement pstmt1;
                     if (event.getEmoji().equalsEmoji(EmojiParser.parseToUnicode(":arrow_left:")) && event.getUser().getId() == user.getId()) { //left arrow
                         if (offset.getOffset() != 0) {
@@ -47,10 +51,11 @@ public class InventoryCommand implements CommandExecutor {
                             pstmt1.setInt(2, limit);
                             pstmt1.setInt(3, offset.getOffset());
                             ResultSet rs1 = pstmt1.executeQuery();
-                            result1 = "";
+                            result1 = "```css\n";
                             while (rs1.next()) {
-                                result1 += rs1.getString("PKMN_NAME") + " x" + rs1.getInt("NB") + "\n";
+                                result1 += "#" + rs1.getString("PKMN") + " " + rs1.getString("PKMN_NAME") + " x" + rs1.getInt("NB") + "\n";
                             }
+                            result1 += "```";
                             msg.edit(result1);
                         }
                     }
@@ -61,12 +66,14 @@ public class InventoryCommand implements CommandExecutor {
                         pstmt1.setInt(2, limit);
                         pstmt1.setInt(3, offset.getOffset());
                         ResultSet rs1 = pstmt1.executeQuery();
-                        result1 = "";
+                        result1 = "```css\n";
                         while (rs1.next()) {
-                            result1 += rs1.getString("PKMN_NAME") + " x" + rs1.getInt("NB") + "\n";
+                            result1 += "#" + rs1.getString("PKMN") + " " + rs1.getString("PKMN_NAME") + " x" + rs1.getInt("NB") + "\n";
                         }
-                        if(!result1.contentEquals(""))
+                        if(!result1.contentEquals("```\n")) {
+                            result1 += "```";
                             msg.edit(result1);
+                        }
                         else
                             offset.minusOffset(limit);
                     }
