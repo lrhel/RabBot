0c6e92a8e505fc365347577b88b14dfb6938e32e
lrhel <lrhel@users.noreply.github.com>
2018-08-04 01:57:48 +0000

    updating and restarting script

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 git pull
 mvn clean install
 pkill java
-java -cp target/RabBot-0.0.1-jar-with-dependencies.jar com.github.lrhel.rabbot.Main >> rabbot.log
+java -cp target/RabBot-0.0.1.jar com.github.lrhel.rabbot.Main >> rabbot.log
