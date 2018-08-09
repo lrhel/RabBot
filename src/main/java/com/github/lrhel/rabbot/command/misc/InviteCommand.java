3d406ccaf475c8ad5e44a78094165f3c4d27cc5d
lrhel <lrhel@users.noreply.github.com>
2018-08-09 03:00:03 +0200

    . . .

--- src/main/java/com/github/lrhel/rabbot/command/misc/InviteCommand.java
+++ src/main/java/com/github/lrhel/rabbot/command/misc/InviteCommand.java
@@ -5,7 +5,7 @@ import de.btobastian.sdcf4j.CommandExecutor;
 import org.javacord.api.DiscordApi;
 
 public class InviteCommand implements CommandExecutor {
-    @Command(aliases = {"invite", "invites", "inv"})
+    @Command(aliases = {"invite", "invites"})
     public String onInviteCommand(DiscordApi api) {
         return api.createBotInvite();
     }
