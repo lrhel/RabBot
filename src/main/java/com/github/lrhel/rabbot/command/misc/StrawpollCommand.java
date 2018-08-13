638094517ca2581b7cc61071fd47f09c3e245b63
lrhel <lrhel@users.noreply.github.com>
2018-08-13 07:04:06 +0200

    StrawPoll: Check

--- src/main/java/com/github/lrhel/rabbot/command/misc/StrawpollCommand.java
+++ src/main/java/com/github/lrhel/rabbot/command/misc/StrawpollCommand.java
@@ -14,119 +14,188 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class StrawpollCommand implements CommandExecutor {
-	@Command(aliases = {"strawpoll"}, privateMessages = false, description = "Make a strawpoll!", showInHelpPage = false, async = true)
-	public String onStrawpollCommand(User user, String[] args, TextChannel textChannel) {
-		if(args.length == 0) {
-			return showHelpMessage();
-		}
+    @Command(aliases = {"strawpoll", "sp"}, privateMessages = false, description = "Make a strawpoll!", showInHelpPage = false, async = true)
+    public String onStrawpollCommand(User user, String[] args, TextChannel textChannel) {
+        if(args.length == 0) {
+            return showHelpMessage();
+        }
 
-		switch (args[0].toLowerCase()) {
-			case "new":
-			case "add":
-				strawpoll(user, textChannel);
-				break;
-			case "check":
-				check(user, textChannel, args);
-				break;
-			default:
-				return showHelpMessage();
-		}
+        switch (args[0].toLowerCase()) {
+            case "new":
+            case "add":
+                strawpoll(user, textChannel);
+                break;
+            case "check":
+            case "get":
+                check(user, textChannel, args);
+                break;
+            default:
+                return showHelpMessage();
+        }
 
-		return "";
-	}
+        return "";
+    }
 
-	private String showHelpMessage() {
-		return "**Usage:** ```rb.strawpoll [new | check [id]]]```";
-	}
+    private String showHelpMessage() {
+        return "**Usage:** ```rb.strawpoll [new | check [id]]```";
+    }
 
-	private void strawpoll(User user, TextChannel textChannel) {
-		AtomicReference<String> title = new AtomicReference<>();
-		AtomicReference<ArrayList<String>> option = new AtomicReference<>(new ArrayList<>(3));
-		AtomicReference<Boolean> waiting = new AtomicReference<>(true);
-		AtomicReference<ListenerManager> listenerManager = new AtomicReference<>();
-		AtomicReference<Boolean> breaking = new AtomicReference<>(false);
-		StrawPoll strawPoll;
-		StringBuilder response = new StringBuilder();
+    private String showHelpMessageCheck() {
+        return "**Usage:** ```rb.strawpoll [check] [id]```";
+    }
 
-		textChannel.sendMessage("The titel of the Strawpoll?").join();
-		listenerManager.set(textChannel.addMessageCreateListener(messageCreateEvent -> {
-			Message message = messageCreateEvent.getMessage();
-			if(message.getUserAuthor().isPresent() && message.getUserAuthor().get().getId() == user.getId()) {
-				title.set(message.getContent());
-				waiting.set(false);
-				listenerManager.get().remove();
-			}
-		}).removeAfter(3, TimeUnit.MINUTES));
-		listenerManager.get().addRemoveHandler(() -> waiting.set(false));
 
-		while (waiting.get()) { Thread.onSpinWait(); }
+    private void strawpoll(User user, TextChannel textChannel) {
+        AtomicReference<String> title = new AtomicReference<>();
+        AtomicReference<ArrayList<String>> option = new AtomicReference<>(new ArrayList<>(3));
+        AtomicReference<Boolean> waiting = new AtomicReference<>(true);
+        AtomicReference<ListenerManager> listenerManager = new AtomicReference<>();
+        AtomicReference<Boolean> breaking = new AtomicReference<>(false);
+        StrawPoll strawPoll;
+        StringBuilder response = new StringBuilder();
 
-		//Option part
-		textChannel.sendMessage("Now we will handle the options, say `STOP` if you don't want to add any more options");
-		for(int i = 1; i <= 50; i++) {
-			AtomicReference<Boolean> forWaiting = new AtomicReference<>(true);
-			textChannel.sendMessage("Option #" + i + " ?").join();
-			listenerManager.set(textChannel.addMessageCreateListener(messageCreateEvent -> {
-				Message message = messageCreateEvent.getMessage();
-				if(message.getUserAuthor().isPresent() && message.getUserAuthor().get().getId() == user.getId()) {
-					if (message.getContent().equalsIgnoreCase("stop")) {
-						breaking.set(true);
-					}
-					else {
-						option.get().add(message.getContent());
-					}
-					forWaiting.set(false);
-					listenerManager.get().remove();
-				}
-			}).removeAfter(3, TimeUnit.MINUTES));
-			listenerManager.get().addRemoveHandler(() -> forWaiting.set(false));
+        textChannel.sendMessage("The titel of the Strawpoll?").join();
+        listenerManager.set(textChannel.addMessageCreateListener(messageCreateEvent -> {
+            Message message = messageCreateEvent.getMessage();
+            if(message.getUserAuthor().isPresent() && message.getUserAuthor().get().getId() == user.getId()) {
+                title.set(message.getContent());
+                waiting.set(false);
+                listenerManager.get().remove();
+            }
+        }).removeAfter(3, TimeUnit.MINUTES));
+        listenerManager.get().addRemoveHandler(() -> waiting.set(false));
 
-			while (forWaiting.get()) { Thread.onSpinWait(); }
+        while (waiting.get()) { Thread.onSpinWait(); }
 
-			if(breaking.get()) {
-				break;
-			}
-		}
+        //Option part
+        textChannel.sendMessage("Now we will handle the options, say `STOP` if you don't want to add any more options");
+        for(int i = 1; i <= 50; i++) {
+            AtomicReference<Boolean> forWaiting = new AtomicReference<>(true);
+            textChannel.sendMessage("Option #" + i + " ?").join();
+            listenerManager.set(textChannel.addMessageCreateListener(messageCreateEvent -> {
+                Message message = messageCreateEvent.getMessage();
+                if(message.getUserAuthor().isPresent() && message.getUserAuthor().get().getId() == user.getId()) {
+                    if (message.getContent().equalsIgnoreCase("stop")) {
+                        breaking.set(true);
+                    }
+                    else {
+                        option.get().add(message.getContent());
+                    }
+                    forWaiting.set(false);
+                    listenerManager.get().remove();
+                }
+            }).removeAfter(3, TimeUnit.MINUTES));
+            listenerManager.get().addRemoveHandler(() -> forWaiting.set(false));
 
-		if(option.get().size() == 0) {
-			return;
-		}
+            while (forWaiting.get()) { Thread.onSpinWait(); }
 
-		//Creating the Strawpoll part
-		strawPoll = new StrawPoll(title.get(), option.get());
-		strawPoll.create();
+            if(breaking.get()) {
+                break;
+            }
+        }
 
-		response.append("Your StrawPoll has been generated\n")
-				.append("```\n")
+        if(option.get().size() == 0) {
+            return;
+        }
 
-				.append("ID: ")
-				.append(strawPoll.getId())
-				.append("\n")
+        //Creating the Strawpoll part
+        strawPoll = new StrawPoll(title.get(), option.get());
+        strawPoll.create();
 
-				.append("Title: ")
-				.append(strawPoll.getTitle())
-				.append("\n")
-		;
+        response.append("Your StrawPoll has been generated\n")
+                .append("\n")
 
-		int i = 1;
-		for(String options : strawPoll.getOptions()) {
-			response.append("Option #")
-					.append(i)
-					.append(": ")
-					.append(options)
-					.append("\n")
-			;
-			i++;
-		}
+                .append("**ID: **")
+                .append(strawPoll.getId())
+                .append("\n")
 
-		response.append("```")
+                .append("**Title: **")
+                .append(strawPoll.getTitle())
+                .append("\n")
 
-				.append("URL: ")
-				.append(strawPoll.getPollURL())
-				.append("\n")
-		;
+        .append("**Options: **\n```")
+        ;
 
-		textChannel.sendMessage(response.toString());
-	}
-	private void check(User user, TextChannel textChannel, String[] args) {}
+        int i = 1;
+        for(String options : strawPoll.getOptions()) {
+            response.append("Option #")
+                    .append(i)
+                    .append(": ")
+                    .append(options)
+                    .append("\n")
+            ;
+            i++;
+        }
+
+        response.append("```")
+
+                .append("**URL: **`")
+                .append(strawPoll.getPollURL())
+                .append("`\n")
+        ;
+
+        textChannel.sendMessage(response.toString());
+    }
+
+    private void check(User user, TextChannel textChannel, String[] args) {
+        StrawPoll strawPoll;
+        ArrayList<Integer> result;
+        ArrayList<String> options;
+        StringBuilder response = new StringBuilder();
+
+        if (args.length == 2) {
+            try {
+                int id = Integer.parseInt(args[1]);
+                strawPoll = new StrawPoll(id);
+                options = new ArrayList<>(strawPoll.getOptions());
+                result = new ArrayList<>(strawPoll.getVotes());
+
+                response.append("**ID: **")
+                        .append(strawPoll.getId())
+                        .append("\n")
+
+                        .append("**Title: **")
+                        .append(strawPoll.getTitle())
+                        .append("\n")
+
+                        .append("**Total voters: **")
+                        .append(result.stream().mapToInt(Integer::intValue).sum())
+                        .append("\n")
+
+                        .append("**Options: **\n```")
+                ;
+
+                for (int i = 0; i < options.size(); i++) {
+                    double percent = ((double) result.get(i) / (double) result.stream().mapToInt(Integer::intValue).sum()) * 100.0;
+
+                    response
+                            .append(options.get(i))
+                            .append(": ")
+                    ;
+                    for (int j = 32 - options.get(i).length(); j > 0; j--) {
+                        response.append(" ");
+                    }
+                    response
+                            .append(percent < 10 ? " " + percent : percent)
+                            .append("% (")
+                            .append(result.get(i))
+                            .append(" voters)\n")
+                    ;
+
+                }
+
+                response.append("```\n**URL:** `")
+                        .append(strawPoll.getPollURL())
+                        .append("`\n")
+                ;
+
+                textChannel.sendMessage(response.toString());
+
+            } catch (NumberFormatException ignored) { }
+        }
+        else {
+            textChannel.sendMessage(showHelpMessageCheck());
+        }
+
+    }
 }
