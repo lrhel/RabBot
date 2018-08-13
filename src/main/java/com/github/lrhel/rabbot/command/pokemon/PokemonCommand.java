47e07037f6a8a427c909561f119ff77155d0dc7d
lrhel <lrhel@users.noreply.github.com>
2018-08-13 20:06:23 +0200

    PokeApi caching System

--- src/main/java/com/github/lrhel/rabbot/command/pokemon/PokemonCommand.java
+++ src/main/java/com/github/lrhel/rabbot/command/pokemon/PokemonCommand.java
@@ -28,8 +28,8 @@ import static com.github.lrhel.rabbot.utility.Utility.getMessageDeleter;
 
 
 public class PokemonCommand implements CommandExecutor {
-    public static int TOTAL_PKMN = 802;
-    private static int INTERVAL = 30 * 1000;
+    public static int TOTAL_PKMN = 10;
+    private static int INTERVAL = 10 * 1000;
     private static int SHINY_RATE = 4096;
 
     private static ArrayList<User> playing = new ArrayList<>(10);
@@ -41,16 +41,19 @@ public class PokemonCommand implements CommandExecutor {
     }
 
     @Command(aliases = {"pokemon", "pkmn"}, description = "Catch a Pokemon", async = true)
-    public void onPokemonCommand(User user, TextChannel textChannel){
+    public void onPokemonCommand(User user, TextChannel textChannel) {
         PokeApi pokeApi = new PokeApiClient();
         Random rng = new Random(System.currentTimeMillis());
-        Pokemon pokemon;
+        Pokemon pokemon = null;
+        RabbotPokemon rabbotPokemon;
         String url = "https://discord.gg/N5c9zfP";
         String name;
         int now = (int) (System.currentTimeMillis() % Integer.MAX_VALUE);
         ExtendedBoolean shiny = new ExtendedBoolean(false);
+        ExtendedBoolean hasVoted = new ExtendedBoolean(false);
+        int pokemonId = rng.nextInt(TOTAL_PKMN) + 1;
 
-        if(playing.contains(user)) {
+        if (playing.contains(user)) {
             return;
         }
 
@@ -62,80 +65,101 @@ public class PokemonCommand implements CommandExecutor {
             int second = (timestamp - now + INTERVAL) / 1000;
             textChannel.sendMessage("Try again in " + second + " seconds").thenAccept(getMessageDeleter(5, TimeUnit.SECONDS));
             playing.remove(user);
-            return ;
+            return;
         }
 
-        try {
-            pokemon = pokeApi.getPokemon(rng.nextInt(TOTAL_PKMN) + 1);
-        } catch (Exception e) {
-            textChannel.sendMessage("Sorry... The PokeAPI is down...").thenAccept(getMessageDeleter(5, TimeUnit.SECONDS));
-            playing.remove(user);
-            return ;
+        rabbotPokemon = RabbotPokemon.getPokemon(pokemonId);
+        if (rabbotPokemon == null) {
+            try {
+                pokemon = pokeApi.getPokemon(pokemonId);
+            } catch (Exception e) {
+                textChannel.sendMessage("Sorry... The PokeAPI is down...").thenAccept(getMessageDeleter(5, TimeUnit.SECONDS));
+                playing.remove(user);
+                return;
+            }
         }
+
 
         if(rng.nextInt(SHINY_RATE) == 444) {
             shiny.set(true);
-        } else {
+        }
+        else {
             discordBotListAPI.hasVoted(user.getIdAsString()).whenComplete((aBoolean, throwable) -> {
                 if (aBoolean.booleanValue()) {
-                    if (rng.nextInt(SHINY_RATE) >= 4000) {
+                    if (rng.nextInt(SHINY_RATE) >= 3700) {
                         shiny.set(true);
                     }
+                    hasVoted.set(true);
                 }
             }).toCompletableFuture().join();
         }
 
-        name = shiny.is() ? "Shiny " + firstUpper(pokemon.getName()) : firstUpper(pokemon.getName());
+        if (rabbotPokemon == null && pokemon != null) {
+            name = shiny.is() ? "Shiny " + firstUpper(pokemon.getName()) : firstUpper(pokemon.getName());
 
-        try {
-            addPokemon(user, pokemon, shiny.is());
+            try {
+                addPokemon(user, pokemon, shiny.is());
 
-        } catch (Exception e) { e.printStackTrace(); }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            name = shiny.is() ? "Shiny " + firstUpper(rabbotPokemon.getPokemonName()) : firstUpper(rabbotPokemon.getPokemonName());
+
+            try {
+                addPokemon(user, rabbotPokemon, shiny.is());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
         String flavour = "";
 
-        Optional<PokemonSpeciesFlavorText> flavourOptional = pokeApi.getPokemonSpecies(pokemon.getId()).getFlavorTextEntries().stream()
-                .filter(f -> f.getLanguage().component1().equalsIgnoreCase("en"))
-                .findAny();
-        if (flavourOptional.isPresent()) {
-            flavour = flavourOptional.get().getFlavorText();
+        if (rabbotPokemon == null) {
+            Optional<PokemonSpeciesFlavorText> flavourOptional = pokeApi.getPokemonSpecies(pokemon.getId()).getFlavorTextEntries().stream()
+                    .filter(f -> f.getLanguage().component1().equalsIgnoreCase("en"))
+                    .findAny();
+            if (flavourOptional.isPresent()) {
+                flavour = flavourOptional.get().getFlavorText();
+            }
+        }
+        else {
+            flavour = rabbotPokemon.getPokedex();
         }
 
         AtomicReference<ListenerManager> lm = new AtomicReference<>();
         AtomicReference<ListenerManager> lm2 = new AtomicReference<>();
 
-        //The different Embeds
+        /**************************
+         *                        *
+         * The different Embeds   *
+         *                        *
+         **************************/
         EmbedBuilder catchPokemonEmbed = new EmbedBuilder()
                 .setTitle("You've caught a Pokemon!!")
                 .setDescription("You've caught a " + name)
-                .setAuthor("Congratulation " + user.getName() + "!!", url, "https://image.noelshack.com/fichiers/2018/29/1/1531772093-pokeball.png");
-        if (shiny.is()) {
-            catchPokemonEmbed.setThumbnail(pokemon.getSprites().getFrontShiny());
-        } else {
-            catchPokemonEmbed.setThumbnail(pokemon.getSprites().getFrontDefault());
-        }
+                .setAuthor("Congratulation " + user.getName() + "!!", url, "https://image.noelshack.com/fichiers/2018/29/1/1531772093-pokeball.png")
+                ;
+        setEmbedThumbnail(pokemon, rabbotPokemon, shiny, catchPokemonEmbed);
+        setFooter(catchPokemonEmbed, hasVoted);
 
         EmbedBuilder pokedexEntryEmbed = new EmbedBuilder()
                 .setTitle("Pokedex entry for " + name)
                 .setDescription(flavour)
-                .setAuthor("Congratulation " + user.getName() + "!!", url, "https://image.noelshack.com/fichiers/2018/29/1/1531772093-pokeball.png");
-
-        if(shiny.is()) {
-            pokedexEntryEmbed.setThumbnail(pokemon.getSprites().getFrontShiny());
-        } else {
-            pokedexEntryEmbed.setThumbnail(pokemon.getSprites().getFrontDefault());
-        }
+                .setAuthor("Congratulation " + user.getName() + "!!", url, "https://image.noelshack.com/fichiers/2018/29/1/1531772093-pokeball.png")
+                ;
+        setEmbedThumbnail(pokemon, rabbotPokemon, shiny, pokedexEntryEmbed);
+        setFooter(pokedexEntryEmbed, hasVoted);
 
         EmbedBuilder finalEmbed = new EmbedBuilder()
                 .setTitle(name)
                 .setDescription("")
-                .setAuthor("Congratulation " + user.getName() + "!!", url, "https://image.noelshack.com/fichiers/2018/29/1/1531772093-pokeball.png");
-
-        if (shiny.is()) {
-            finalEmbed.setThumbnail(pokemon.getSprites().getFrontShiny());
-        } else {
-            finalEmbed.setThumbnail(pokemon.getSprites().getFrontDefault());
-        }
+                .setAuthor("Congratulation " + user.getName() + "!!", url, "https://image.noelshack.com/fichiers/2018/29/1/1531772093-pokeball.png")
+                ;
+        setEmbedThumbnail(pokemon, rabbotPokemon, shiny, finalEmbed);
+        setFooter(finalEmbed, hasVoted);
 
         Message message = textChannel.sendMessage(catchPokemonEmbed).join();
         try {
@@ -158,6 +182,37 @@ public class PokemonCommand implements CommandExecutor {
                 }
             }).removeAfter(5, TimeUnit.MINUTES));
         } catch (Exception ignored) { }
+        if(rabbotPokemon != null) {
+            System.out.println("cached");
+        }
         playing.remove(user);
+    }
+
+    private void setEmbedThumbnail(Pokemon pokemon, RabbotPokemon rabbotPokemon, ExtendedBoolean shiny, EmbedBuilder finalEmbed) {
+        if (shiny.is()) {
+            if(rabbotPokemon == null) {
+                finalEmbed.setThumbnail(pokemon.getSprites().getFrontShiny());
+            }
+            else {
+                finalEmbed.setThumbnail(rabbotPokemon.getImageShiny());
+            }
+        }
+        else {
+            if(rabbotPokemon == null) {
+                finalEmbed.setThumbnail(pokemon.getSprites().getFrontDefault());
+            }
+            else {
+                finalEmbed.setThumbnail(rabbotPokemon.getImage());
+            }
+        }
+    }
+
+    private EmbedBuilder setFooter(EmbedBuilder embedBuilder, ExtendedBoolean extendedBoolean) {
+        if (extendedBoolean.isNot()) {
+            return embedBuilder.setFooter("More chances to catch a Shiny? Try the rb.bonus command");
+        }
+        else {
+            return embedBuilder;
+        }
     }
 }
--- src/main/java/com/github/lrhel/rabbot/command/pokemon/RabbotPokemon.java
+++ src/main/java/com/github/lrhel/rabbot/command/pokemon/RabbotPokemon.java
@@ -1,20 +1,91 @@
 package com.github.lrhel.rabbot.command.pokemon;
 
 import com.github.lrhel.rabbot.sqlite.Sqlite;
+import me.sargunvohra.lib.pokekotlin.client.PokeApi;
+import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
 import me.sargunvohra.lib.pokekotlin.model.Pokemon;
+import me.sargunvohra.lib.pokekotlin.model.PokemonSpeciesFlavorText;
 import org.javacord.api.entity.user.User;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 import static com.github.lrhel.rabbot.sqlite.Sqlite.getTimestampFromSql;
 import static com.github.lrhel.rabbot.utility.Utility.firstUpper;
 
-public interface RabbotPokemon {
-    static void addPokemon(User user, Pokemon pokemon, boolean shiny) throws SQLException {
-        String sql = "INSERT INTO CATCH(DISCORD_ID, PKMN, PKMN_NAME, TIMESTAMP) " +
-                "VALUES(?,?,?,?)";
+public class RabbotPokemon {
+
+    private int pokemonId;
+    private String pokemonName;
+    private String image;
+    private String imageShiny;
+    private String pokedex;
+
+    private RabbotPokemon(int pokemonId, String pokemonName, String image, String imageShiny, String pokedex) {
+        this.pokemonId = pokemonId;
+        this.pokemonName = pokemonName;
+        this.image = image;
+        this.imageShiny = imageShiny;
+        this.pokedex = pokedex;
+    }
+
+    public int getPokemonId() {
+        return pokemonId;
+    }
+
+    public String getPokemonName() {
+        return pokemonName;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getImageShiny() {
+        return imageShiny;
+    }
+
+    public String getPokedex() {
+        return pokedex;
+    }
+
+    public static RabbotPokemon getPokemon(int id) {
+        String sql = "SELECT pkmn, pkmn_name, image, image_shiny, pokedex " +
+                "FROM catch WHERE pkmn = ? AND pkmn_name NOT LIKE '%Shiny%'" +
+                "AND image IS NOT NULL";
+        try {
+            PreparedStatement preparedStatement = Sqlite.getInstance().getConnection().prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()) {
+                String pkmnName = resultSet.getString("pkmn_name");
+                String image = resultSet.getString("image");
+                String imageShiny = resultSet.getString("image_shiny");
+                String pokedex = resultSet.getString("pokedex");
+                return new RabbotPokemon(id, pkmnName, image, imageShiny, pokedex);
+            } else {
+                return null;
+            }
+        } catch (Exception ignored) { ignored.printStackTrace();return null; }
+    }
+
+    public static void addPokemon(User user, Pokemon pokemon, boolean shiny) throws SQLException {
+        String sql = "INSERT INTO CATCH(DISCORD_ID, PKMN, PKMN_NAME, TIMESTAMP, image, image_shiny, pokedex) " +
+                "VALUES(?,?,?,?,?,?,?)";
         StringBuilder pokemonName = new StringBuilder();
+        PokeApi pokeApi = new PokeApiClient();
+
+        String flavour = "";
+
+        Optional<PokemonSpeciesFlavorText> flavourOptional = pokeApi.getPokemonSpecies(pokemon.getId()).getFlavorTextEntries().stream()
+                .filter(f -> f.getLanguage().component1().equalsIgnoreCase("en"))
+                .findAny();
+        if (flavourOptional.isPresent()) {
+            flavour = flavourOptional.get().getFlavorText();
+        }
+
 
         if (shiny) {
             pokemonName.append("Shiny ");
@@ -26,35 +97,59 @@ public interface RabbotPokemon {
         pstmt.setInt(2, pokemon.getId());
         pstmt.setString(3, pokemonName.toString());
         pstmt.setInt(4, Math.toIntExact(System.currentTimeMillis() % Integer.MAX_VALUE));
+        pstmt.setString(5, pokemon.getSprites().getFrontDefault());
+        pstmt.setString(6, pokemon.getSprites().getFrontShiny());
+        pstmt.setString(7, flavour);
         pstmt.executeUpdate();
         pstmt.close();
     }
 
-    static void addPokemon(User user, Pokemon pokemon) throws SQLException {
+    public static void addPokemon(User user, RabbotPokemon pokemon, boolean shiny) throws SQLException {
+        String sql = "INSERT INTO CATCH(DISCORD_ID, PKMN, PKMN_NAME, TIMESTAMP, image, image_shiny, pokedex) " +
+                "VALUES(?,?,?,?,?,?,?)";
+        StringBuilder pokemonName = new StringBuilder();
+
+        if (shiny) {
+            pokemonName.append("Shiny ");
+        }
+        pokemonName.append(firstUpper(pokemon.getPokemonName()));
+
+        PreparedStatement pstmt = Sqlite.getInstance().getConnection().prepareStatement(sql);
+        pstmt.setString(1, user.getIdAsString());
+        pstmt.setInt(2, pokemon.getPokemonId());
+        pstmt.setString(3, pokemonName.toString());
+        pstmt.setInt(4, Math.toIntExact(System.currentTimeMillis() % Integer.MAX_VALUE));
+        pstmt.setString(5, pokemon.getImage());
+        pstmt.setString(6, pokemon.getImageShiny());
+        pstmt.setString(7, pokemon.getPokedex());
+        pstmt.executeUpdate();
+        pstmt.close();
+    }
+
+    public static void addPokemon(User user, Pokemon pokemon) throws SQLException {
         addPokemon(user, pokemon, false);
     }
 
-
-    static int totalCatchedPokemon(User user) throws  SQLException {
+    public static int totalCatchedPokemon(User user) throws  SQLException {
         String sql  = "SELECT COUNT(*) as count FROM catch WHERE discord_id = ?";
         PreparedStatement preparedStatement = Sqlite.getInstance().getConnection().prepareStatement(sql);
         preparedStatement.setString(1, user.getIdAsString());
         return preparedStatement.executeQuery().getInt("count");
     }
 
-    static int totalCatchedPokemon() throws SQLException {
+    public static int totalCatchedPokemon() throws SQLException {
         String sql  = "SELECT COUNT(*) as count FROM catch";
         PreparedStatement preparedStatement = Sqlite.getInstance().getConnection().prepareStatement(sql);
         return preparedStatement.executeQuery().getInt("count");
     }
 
-    static int totalUniqueCatchedPokemon() throws SQLException {
+    public static int totalUniqueCatchedPokemon() throws SQLException {
         String sql = "SELECT COUNT(DISTINCT PKMN_NAME) as count FROM catch";
         PreparedStatement preparedStatement = Sqlite.getInstance().getConnection().prepareStatement(sql);
         return preparedStatement.executeQuery().getInt("count");
     }
 
-    static int totalUniqueCatchedPokemon(User user) throws SQLException {
+    public static int totalUniqueCatchedPokemon(User user) throws SQLException {
         String sql  = "SELECT COUNT(DISTINCT PKMN_NAME) as count FROM catch WHERE discord_id = ?";
         PreparedStatement preparedStatement = Sqlite.getInstance().getConnection().prepareStatement(sql);
         preparedStatement.setString(1, user.getIdAsString());
@@ -66,7 +161,7 @@ public interface RabbotPokemon {
      * @param user
      * @return
      */
-    static int getTimestamp(User user) {
+    public static int getTimestamp(User user) {
         String sql = "SELECT * FROM catch WHERE discord_id = ? ORDER BY id DESC LIMIT 0, 1";
         return getTimestampFromSql(user,  sql);
     }
