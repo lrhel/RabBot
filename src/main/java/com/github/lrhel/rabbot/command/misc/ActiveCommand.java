953227d96d83f639c1828e101dcd844045457c37
lrhel <lrhel@users.noreply.github.com>
2018-10-03 11:51:11 +0000

    :thinking:

--- src/main/java/com/github/lrhel/rabbot/command/misc/ActiveCommand.java
+++ src/main/java/com/github/lrhel/rabbot/command/misc/ActiveCommand.java
@@ -78,10 +78,12 @@ public class ActiveCommand	implements CommandExecutor {
 				ArrayList<Role> userRole = new ArrayList<Role>(message.getMentionedRoles());
 				if(userList.size() != 0 || userRole.size() != 0) {
 					for(User usr : userList) {
-			    		String msg = usr.getMentionTag() + " y so active xdxdxd";
-			    		ch.sendMessage(msg);
-			    		break;				    	
-				    }
+					    for(int alpha =0; alpha < arg_i; i++) {
+						String msg = usr.getMentionTag() + " y so active xdxdxd";
+						ch.sendMessage(msg);
+					    }
+					    break;				    	
+					}
 					for (Role role : userRole) {
 						String msg = role.getMentionTag() + " y so active xdxdxd";
 						ch.sendMessage(msg);
