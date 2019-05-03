7579ee2f5ef104d4bbcf3131ca05fe11bd1ffaea
burdoto <burdoto@outlook.com>
2019-05-03 16:07:21 +0200

    Gradle Port

--- restart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pkill java
-java -cp target/RabBot-0.0.1.jar com.github.lrhel.rabbot.Main >> rabbot.log
--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git pull
-mvn clean install
-pkill java
-java -cp target/RabBot-0.0.1.jar com.github.lrhel.rabbot.Main >> rabbot.log
